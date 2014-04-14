45966067d56ba86ae4379bb23e4a57ee024dd8e0
yuya yano <yuya.yano.6260@gmail.com>
2014-04-14 11:13:12 +0900

    Misc changed.

--- yoichi/__init__.py
+++ yoichi/__init__.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, session
 
 app = Flask(__name__)
+
+app.config.from_object(config)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+#@app.before_request
+#def load_current_user():
+    #g.user = User.query.filter_by()
+
+#@app.teardown_request
+#def remove_db_session(exception):
+    #db_session.remove()
 
 @app.route('/')
 def index():
