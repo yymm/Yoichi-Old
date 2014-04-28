bceb457717975f3377dfd6a83c63b4a03b61036f
yuya yano <yuya.yano.6260@gmail.com>
2014-04-28 13:55:20 +0900

    Change to run via a 5000:port, because suddenly I can not use proxylocal...

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 SECRET_KEY = 'dev key'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'yoichi.db')
 ADMINS = frozenset(['yuyayano'])
+DEBUG = True
 
 del os
--- run.py
+++ run.py
@@ -1,2 +1,11 @@
+import socket
 from yoichi import app
-app.run(debug=True)
+
+host = None
+port = None
+
+if app.config["DEBUG"]:
+    host = socket.gethostbyname('%s.local' % socket.gethostname())
+    port = 5000
+
+app.run(host=host, port=port)
