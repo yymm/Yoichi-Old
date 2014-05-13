7bfab83ea555105db2eae7f28ec7b47379686f2a
yuya yano <yuya.yano.6260@gmail.com>
2014-05-13 23:31:33 +0900

    Added a page guide.

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ from yoichi import app
 host = None
 port = None
 
-if app.config["DEBUG"]:
-    try:
-        host = socket.gethostbyname('%s.local' % socket.gethostname())
-        port = 5000
-    except:
-        host = '127.0.0.1'
-        port = 5000
+#if app.config["DEBUG"]:
+    #try:
+        #host = socket.gethostbyname('%s.local' % socket.gethostname())
+        #port = 5000
+    #except:
+        #host = '127.0.0.1'
+        #port = 5000
 
 app.run(host=host, port=port)
