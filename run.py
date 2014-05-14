610bf1198f1b5e8e98de51024800829cb388dfa5
yuya yano <yuya.yano.6260@gmail.com>
2014-05-14 16:49:54 +0900

    Removed code what added when proxylocal is not used.

--- run.py
+++ run.py
@@ -1,15 +1,4 @@
 import socket
 from yoichi import app
 
-host = None
-port = None
-
-#if app.config["DEBUG"]:
-    #try:
-        #host = socket.gethostbyname('%s.local' % socket.gethostname())
-        #port = 5000
-    #except:
-        #host = '127.0.0.1'
-        #port = 5000
-
 app.run(host=host, port=port)
